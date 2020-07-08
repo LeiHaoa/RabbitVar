725f3b85d0485c7e88a13c750365e233639b0cba
haoz <ideahao@outlook.com>
2020-07-08 13:04:45 +0800

    tune scripts

--- scripts/format_germ.sh
+++ scripts/format_germ.sh
@@ -1,2 +1,3 @@
 AF_THR="0.01"
-cat $1 | ./teststrandbias.R | ./var2vcf_valid.pl -N sample_name -f $AF_THR
+SAMPLE_NAME="CHM1_CHM13"
+cat $1 | ./teststrandbias.R | ./var2vcf_valid.pl -N ${SAMPLE_NAME} -f $AF_THR
--- scripts/somatic_omp_run.sh
+++ scripts/somatic_omp_run.sh
@@ -12,7 +12,8 @@ export KMP_AFFINITY=scatter
 #./launcher /home/old_home/haoz/workspace/data/NA12878/ConfidentRegions.bed -G /home/old_home/haoz/workspace/data/NA12878/hg38.fa -f 0.001 -N sample_name -b "/home/old_home/haoz/workspace/data/NA12878/NA12878_S1.bam|/home/old_home/haoz/workspace/data/NA12877/NA12877_S1.bam" -c 1 -S 2 -E 3 -g 4 --th $1 --out ./somat.vcf
 
 DATA_DIR=/home/old_home/haoz/workspace/data
-../bin/FastVC ${DATA_DIR}/NA12878/ConfidentRegions.bed \
+../bin/FastVC \
+	-i ${DATA_DIR}/NA12878/ConfidentRegions.bed \
 	-G ${DATA_DIR}/hg38/hg38.fa \
 	-f 0.001 \
 	-N "HCC1187C|HCC1187BL" \
