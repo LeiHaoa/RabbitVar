98be03c3d107720c53306165ad49b93f730d30b7
haoz <ideahao@outlook.com>
2021-03-29 22:07:38 +0800

    format code and add utils to code reuse

--- RandomForest/train_rf.py
+++ RandomForest/train_rf.py
@@ -1,5 +1,7 @@
 import sys
+import os
 from multiprocessing import Process, Queue
+sys.path.append(os.path.join(os.getcwd(), '../utils'))
 
 import joblib
 import numpy as np
@@ -10,34 +12,10 @@ import sklearn.model_selection
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.utils import shuffle
 import argparse
-import os
 
-som_features = [
-    "Sample", "Gene", "Chr", "Start", "End", "Ref", "Alt",
-    "Var1Depth", "Var1AltDepth", "Var1RefFwdReads", "Var1RefRevReads", "Var1AltFwdReads", "Var1AltRevReads", "Var1Genotype", "Var1AF",
-    "Var1Bias", "Var1PMean", "Var1PStd", "Var1QMean", "Var1QStd", "Var1MQ", "Var1Sig_Noise", "Var1HiAF", "Var1ExtraAF", "Var1NM", "Var1Pvalue", "Var1Oddr",
-    "Var2Depth", "Var2AltDepth", "Var2RefFwdReads", "Var2RefRevReads", "Var2AltFwdReads", "Var2AltRevReads", "Var2Genotype", "Var2AF",
-    "Var2Bias", "Var2PMean", "Var2PStd", "Var2QMean", "Var2QStd", "Var2MQ", "Var2Sig_Noise", "Var2HiAF", "Var2ExtraAF", "Var2NM", "Var2Pvalue", "Var2Oddr",
-    "shift3", "MSI", "MSI_NT", "5pFlankSeq", "3pFlankSeq", "Seg", "VarLabel", "VarType",
-    "Duprate1", "SV_info1", "Duprate2", "SV_info2", "Pvalue", "Oddratio"
-    ]
-som_features_to_index = {
-    "Sample":0, "Gene":1, "Chr":2, "Start":3, "End":4, "Ref":5, "Alt":6,
-    "Var1Depth":7, "Var1AltDepth":8, "Var1RefFwdReads":9, "Var1RefRevReads":10, "Var1AltFwdReads":11, "Var1AltRevReads":12, "Var1Genotype":13, "Var1AF":14,
-    "Var1Bias":15, "Var1PMean":16, "Var1PStd":17, "Var1QMean":18, "Var1QStd":19, "Var1MQ":20, "Var1Sig_Noise":21, "Var1HiAF":22, "Var1ExtraAF":23, "Var1NM":24, "Var1Pvalue":25, "Var1Oddr":26,
-    "Var2Depth":27, "Var2AltDepth":28, "Var2RefFwdReads":29, "Var2RefRevReads":30, "Var2AltFwdReads":31, "Var2AltRevReads":32, "Var2Genotype":33, "Var2AF":34,
-    "Var2Bias":35, "Var2PMean":36, "Var2PStd":37, "Var2QMean":38, "Var2QStd":39, "Var2MQ":40, "Var2Sig_Noise":41, "Var2HiAF":42, "Var2ExtraAF":43, "Var2NM":44, "Var2Pvalue":45, "Var2Oddr":46,
-    "shift3":47, "MSI":48, "MSI_NT":49, "5pFlankSeq":50, "3pFlankSeq":51, "Seg":52, "VarLabel":53, "VarType":54,
-    "Duprate1":55, "SV_info1":56, "Duprate2":57, "SV_info2":58, "Pvalue":59, "Oddratio":60
-    }
-som_selected_features = [
-    "Var1Depth", "Var1AltDepth", "Var1RefFwdReads", "Var1RefRevReads", "Var1AltFwdReads", "Var1AltRevReads", "Var1AF",
-    "Var1PMean", "Var1PStd", "Var1QMean", "Var1QStd", "Var1MQ", "Var1Sig_Noise", "Var1HiAF", "Var1ExtraAF", "Var1NM", "Var1Pvalue", "Var1Oddr",
-    "Var2Depth", "Var2AltDepth", "Var2RefFwdReads", "Var2RefRevReads", "Var2AltFwdReads", "Var2AltRevReads", "Var2AF",
-    "Var2PMean", "Var2PStd", "Var2QMean", "Var2QStd", "Var2MQ", "Var2Sig_Noise", "Var2HiAF", "Var2ExtraAF", "Var2NM", "Var2Pvalue", "Var2Oddr",
-    "shift3", "MSI", "MSI_NT",
-    "Pvalue", "Oddratio"
-    ]
+from features import *
+from datautil import get_data_fromcsv, hard_filter
+
 SOM_SNV_FEATURES=len(som_selected_features) + 1 
 SOM_INDEL_FEATURES = SOM_SNV_FEATURES + 3
 
@@ -46,6 +24,7 @@ varLabel_to_label = {
     "Germline":0, "StrongLOH":1, "LikelyLOH":2, "StrongSomatic":3,
     "LikelySomatic":4, "AFDiff":5, "SampleSpecific":6
 }
+
 fe2i = som_features_to_index
 fvc_sf = som_selected_features
 def format_snv_data_item(jri, fisher):
@@ -90,7 +69,7 @@ def get_label(truth_vars, key):
         return 0
     return 0
 
-def train_rf(args):
+def train_rf_old(args):
     #----prepare label list----#
     #truth_file = "/home/old_home/haoz/workspace/data/FD/Truth/FDtruth_Data_1.snv.vcf"
     truth_file = args.truth_file
@@ -190,9 +169,7 @@ def train_rf(args):
 
         #--select frac faslse data form fasle
         false = train[ train['label'] == 0 ]
-        #false = false.sample(frac = 0.05)
-        #false = false.sample(frac = 0.5)
-        truth = train[ train['label'] == 1]
+        truth = train[ train['label'] == 1 ]
         print("faslse number: {}, truth number: {}".format(len(false), len(truth)*5 ) )
         aug_data = shuffle(pd.concat([truth, false, truth, truth, truth, truth], axis = 0))
         #aug_data = train
@@ -228,6 +205,69 @@ def train_rf(args):
     ypred = clf.predict(test_set)
     print("Accuracy score", sklearn.metrics.accuracy_score(ytest, ypred))
 
+def train_rf(args):
+    #----prepare label list----#
+    truth_file = args.truth_file
+    vartype = args.var_type
+    #--- prepare source datafram from file ---#
+    fastvc_file = args.train_data
+    cr = list()
+
+    if vartype == "SNV":
+        if args.tsv == None:
+            data = pd.DataFrame(cr, columns=[*som_selected_features, "VarLabel", "label"])
+            data[data.columns] = data[data.columns].apply(pd.to_numeric)
+            #data['VarLabel'] = data['VarLabel'].astype('category')
+            #data["label"] = data["label"]
+        else:
+            data = get_data_fromcsv(args.tsv, [*som_selected_features, "VarLabel", "label"], vtype = 'SNV')
+
+        data = hard_filter(data)
+
+        #--- data prepare ----#
+        false = data[ data['label'] == 0 ]
+        truth = data[ data['label'] == 1 ]
+        print("faslse number: {}, truth number: {}".format(len(false), len(truth)) )
+        aug_data = shuffle(pd.concat([truth, false, truth, truth], axis = 0))
+        y = aug_data["label"]#.to_numpy()
+        print(len(aug_data), len(y))
+    elif vartype == "INDEL":
+        if args.tsv == None:
+            data = pd.DataFrame(cr, columns=["RefLength", "AltLength", "VarType", *som_selected_features, "VarLabel", "label"])
+            data[data.columns[:-2]] = data[data.columns[:-2]].apply(pd.to_numeric)
+            #data["label"] = data["label"].astype('category')
+            data[data.columns] = data[data.columns].apply(pd.to_numeric)
+        else:
+            data = get_data_fromcsv(args.tsv, ["RefLength", "AltLength", "VarType", *som_selected_features, "VarLabel", "label"], vtype = 'SNV')
+
+        data = hard_filter(data)
+        #--- data prepare ----#
+        false = data[ data['label'] == 0 ]
+        truth = data[ data['label'] == 1 ]
+        print("faslse number: {}, truth number: {}".format(len(false), len(truth)) )
+        aug_data = shuffle(pd.concat([truth, false, truth, truth], axis = 0))
+        y = aug_data["label"]#.to_numpy()
+        print(len(aug_data), len(y))
+
+    print("data prepare done, start fiting ...")
+    if args.pretrained_model == None:
+        clf = RandomForestClassifier(n_jobs=args.nthreads, max_depth=20, min_samples_leaf=50, 
+                                     n_estimators=150, max_features=None)
+    else:
+        print("model {} exists!".format(args.pretrained_model))
+        exit(-1)
+        if not os.path.exists(args.pretrained_model):
+            print("model {} not exists, create new one!".format(args.pretrained_model))
+            clf = RandomForestClassifier(n_jobs=args.nthreads, max_depth=20, min_samples_leaf=50, 
+                                         n_estimators=150, max_features=None)
+        else:
+            print("loading model: ", args.pretrained_model)
+            clf = joblib.load(args.pretrained_model)
+
+    clf.fit(aug_data, y)
+    
+    joblib.dump(clf, args.out_model, compress=9)
+    print("store model done")
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description = "train your network")
