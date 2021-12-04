8c8458bc917bcc4f2ad64f285df4addfa9c1744b
LeiHaoa <haoz@mail.sdu.edu.cn>
2021-12-04 02:59:14 +0000

    change intersection model

--- scripts/get_intersection.py
+++ scripts/get_intersection.py
@@ -41,6 +41,8 @@ def get_info_vardict(var):
   return tuple((chrom, start, ref,
                 alt, vaf))
 
+NOINDEL=False
+NOSNV=False
 def get_vcf_info(var):
   #chrom start end ref alt gene vaf
   var = var.split('\t')
@@ -54,15 +56,23 @@ def get_vcf_info(var):
   start = var[1]
   ref = var[3]
   alt = var[4]
+  #snv:
+  if NOSNV and len(ref) == 1 and len(alt) == 1:
+    return ""
+
+  if NOINDEL and (len(ref) > 1 or len(alt) > 1):
+    return ""
+  
   #--case: insertion
-  if len(ref) < len(alt):
-      ref = "."
-      alt = alt[1:]
-  elif len(ref) > len(alt):
-      start = str(int(start) + 1)
-      alt = "."
-      ref = ref[1:]
+  # if len(ref) < len(alt):
+  #     ref = "."
+  #     alt = alt[1:]
+  # elif len(ref) > len(alt):
+  #     start = str(int(start) + 1)
+  #     alt = "."
+  #     ref = ref[1:]
   #return(chrom, start, ref, alt, vaf)                
+  #print("debug:", chrom+":"+start+":"+ref+":"+alt)
   return chrom+":"+start+":"+ref+":"+alt
 
 def get_variant_set(filename):
@@ -80,25 +90,33 @@ def get_intersection_num(s1, s2):
       counter += 1
   return counter
 
+def padding(s):
+  return s + ''.join((2-len(s)//8)*'\t')
+
 def main():
-  vardict_vcf_file = "/home/haoz/data/tmpvcf.vcf"
-  mutect_vcf_file = "/home/haoz/data/tmpvcf.vcf"
-  strelka_vcf_file = "/home/haoz/data/tmpvcf.vcf"
-  mc2v = {'mutect2':set(), 'vardict':set(), 'strelka2':set()} #map: caller to vairants set
+  #vardict_vcf_file = "/home/user_home/haoz/workspace/RabbitVar/benchresults/RabVar_T75N05.vcf"
+  #mutect_vcf_file = "/home/user_home/haoz/bcbio_bench_result/FDSyn_impure/T75N05_work/final_mutect2/SPP_GT_3-1_1/SPP_GT_3-1_1-mutect2.vcf"
+  #strelka_vcf_file = "/home/user_home/haoz/bcbio_bench_result/FDSyn_impure/T75N05_work/final_strelka2/SPP_GT_3-1_1/SPP_GT_3-1_1-strelka2.vcf"
+  vardict_vcf_file = "/home/user_home/haoz/workspace/RabbitVar/benchresults/RabVar_FD2.vcf"
+  mutect_vcf_file = "/home/user_home/haoz/workspace/Experiments/variant_calling/FD_T2-mutect2.vcf"
+  strelka_vcf_file = "/home/user_home/haoz/workspace/Experiments/variant_calling/FD_T2-strelka2.vcf"
+  ground_truth_file = "/home/user_home/haoz/data/FD/Truth/FDALL_TRUTH.vcf"
+  mc2v = {'mutect2':set(), 'vardict':set(), 'strelka2':set(), 'gt':set()} #map: caller to vairants set
   #map: caller to file name
-  mc2f = {'mutect2':mutect_vcf_file, 'vardict':vardict_vcf_file, 'strelka2':strelka_vcf_file} 
+  mc2f = {'mutect2':mutect_vcf_file, 'vardict':vardict_vcf_file, 'strelka2':strelka_vcf_file, 'gt':ground_truth_file} 
   callers = mc2v.keys()
   for c in callers:
     mc2v[c] = get_variant_set(mc2f[c])
 
   for i in callers:
-    line = ""
+    line = padding(i)
+    base = len(mc2v[i])
     for j in callers:
       if i == j: 
-        line += "0\t" 
+        line += "{}\t".format(len(mc2v[i]) / base) 
         continue
       #print("length of caller {} and {} set: {} {}".format(i, j, len(mc2v[i]), len(mc2v[j])))
-      line += str(get_intersection_num(mc2v[i], mc2v[j]))
+      line += str(get_intersection_num(mc2v[i], mc2v[j]) / base)
       line += '\t'
     line += '\n'
     print(line)
